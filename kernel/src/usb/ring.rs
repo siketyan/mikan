bd53dcdb2072f9912234716697d4bc03a6b5fa20
Natsuki Ikeguchi <me@s6n.jp>
2023-07-02 17:59:09 +0900

    wip: Try no-op command

--- kernel/src/usb/ring.rs
+++ kernel/src/usb/ring.rs
@@ -3,14 +3,20 @@ use core::mem::size_of;
 
 use num_traits::FromPrimitive;
 use xhci::ring::trb::event::{CommandCompletion, PortStatusChange, TransferEvent};
-use xhci::ring::trb::Type;
+use xhci::ring::trb::{Link, Type};
 
 use crate::println;
 use crate::usb::memory::Pool;
 use crate::usb::Controller;
 
+#[repr(C, packed)]
+#[derive(Debug, Default, Copy, Clone)]
+pub(crate) struct Trb {
+    data: [u32; 4],
+}
+
 #[derive(Debug)]
-enum TrbPayload<'t> {
+enum EventTrbPayload<'t> {
     TransferEvent(&'t TransferEvent),
     PortStatusChange(&'t PortStatusChange),
     CommandCompletion(&'t CommandCompletion),
@@ -18,13 +24,13 @@ enum TrbPayload<'t> {
 
 #[repr(C, packed)]
 #[derive(Debug, Default, Copy, Clone)]
-struct Trb {
+struct EventTrb {
     _dummy2: u64,
     _dummy: u32,
     bit: u32,
 }
 
-impl Trb {
+impl EventTrb {
     fn cycle_bit(&self) -> bool {
         (self.bit & 1 << 0) != 0
     }
@@ -38,11 +44,11 @@ impl Trb {
         unsafe { &*(self as *const Self as *const T) }
     }
 
-    fn payload(&self) -> TrbPayload {
+    fn payload(&self) -> EventTrbPayload {
         match self.ty() {
-            Type::TransferEvent => TrbPayload::TransferEvent(self.cast()),
-            Type::PortStatusChange => TrbPayload::PortStatusChange(self.cast()),
-            Type::CommandCompletion => TrbPayload::CommandCompletion(self.cast()),
+            Type::TransferEvent => EventTrbPayload::TransferEvent(self.cast()),
+            Type::PortStatusChange => EventTrbPayload::PortStatusChange(self.cast()),
+            Type::CommandCompletion => EventTrbPayload::CommandCompletion(self.cast()),
             _ => unimplemented!(),
         }
     }
@@ -58,8 +64,59 @@ struct SegmentTableEntry {
 }
 
 #[derive(Debug)]
-pub(crate) struct EventRing<'r> {
+pub(crate) struct Ring<'r> {
     buffer: &'r mut [Trb],
+    cycle_bit: bool,
+    cursor: usize,
+}
+
+impl<'r> Ring<'r> {
+    pub(crate) fn new(size: usize, controller: &mut Controller) -> Self {
+        let pool = Pool::get();
+        let buffer = pool.allocate_slice(size, 64, 64 * 1024);
+        buffer.fill(Trb::default());
+
+        let ring = Self {
+            buffer,
+            cycle_bit: true,
+            cursor: 0,
+        };
+
+        controller.registers.operational.crcr.update_volatile(|r| {
+            r.set_ring_cycle_state();
+            r.set_command_ring_pointer(ring.buffer.as_mut_ptr() as u64);
+        });
+
+        ring
+    }
+
+    pub(crate) fn copy_to_last(&mut self, data: [u32; 4]) {
+        self.buffer[self.cursor].data = data;
+        self.buffer[self.cursor].data[3] = (data[3] & 0xfffffffe) | self.cycle_bit as u32;
+    }
+
+    pub(crate) fn push(&mut self, data: [u32; 4]) -> *const Trb {
+        self.copy_to_last(data);
+        self.cursor += 1;
+
+        let ptr = &self.buffer[self.cursor] as *const Trb;
+        if self.cursor == self.buffer.len() - 1 {
+            let mut link = Link::new();
+            link.set_ring_segment_pointer(self.buffer.as_mut_ptr() as u64);
+            link.set_toggle_cycle();
+            self.copy_to_last(link.into_raw());
+
+            self.cursor = 0;
+            self.cycle_bit = !self.cycle_bit;
+        }
+
+        ptr
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct EventRing<'r> {
+    buffer: &'r mut [EventTrb],
     cycle_bit: bool,
     segment_table: &'r mut [SegmentTableEntry],
 }
@@ -68,7 +125,7 @@ impl<'r> EventRing<'r> {
     pub(crate) fn new(size: usize, controller: &mut Controller) -> Self {
         let pool = Pool::get();
         let buffer = pool.allocate_slice(size, 64, 64 * 1024);
-        buffer.fill(Trb::default());
+        buffer.fill(EventTrb::default());
 
         let segment_table = Pool::get().allocate_slice(1, 64, 64 * 1024);
         segment_table.fill(SegmentTableEntry::default());
@@ -87,15 +144,15 @@ impl<'r> EventRing<'r> {
             r.set(ring.segment_table.len() as u16);
         });
 
-        println!("ERDP: {:04X}", &ring.buffer[0] as *const Trb as u64);
+        println!("ERDP: {:04X}", &ring.buffer[0] as *const EventTrb as u64);
         ring.write_dequeue_pointer(controller, &ring.buffer[0]);
 
         println!(
             "ERSTBA {:16X}",
-            ring.segment_table as *mut [SegmentTableEntry] as *mut c_void as u64
+            ring.segment_table.as_mut_ptr() as *mut c_void as u64
         );
         controller.interrupter().erstba.update_volatile(|r| {
-            r.set(ring.segment_table as *mut [SegmentTableEntry] as *mut c_void as u64);
+            r.set(ring.segment_table.as_mut_ptr() as *mut c_void as u64);
         });
 
         controller.interrupter().iman.update_volatile(|r| {
@@ -121,8 +178,8 @@ impl<'r> EventRing<'r> {
     }
 
     fn pop(&mut self, controller: &mut Controller) {
-        let mut ptr = unsafe { (self.read_dequeue_pointer(controller) as *const Trb).add(1) };
-        let segment_begin = self.segment_table[0].base as *const Trb;
+        let mut ptr = unsafe { (self.read_dequeue_pointer(controller) as *const EventTrb).add(1) };
+        let segment_begin = self.segment_table[0].base as *const EventTrb;
         let segment_end = unsafe { segment_begin.add(self.segment_table[0].size as usize) };
         if ptr == segment_end {
             ptr = segment_end;
@@ -136,21 +193,23 @@ impl<'r> EventRing<'r> {
         self.front(controller).cycle_bit() == self.cycle_bit
     }
 
-    fn front(&self, controller: &mut Controller) -> Trb {
-        *self.read_dequeue_pointer(controller)
+    fn front(&self, controller: &mut Controller) -> EventTrb {
+        let ptr = self.read_dequeue_pointer(controller);
+
+        return *ptr;
     }
 
-    fn read_dequeue_pointer(&self, controller: &mut Controller) -> &Trb {
+    fn read_dequeue_pointer(&self, controller: &mut Controller) -> &EventTrb {
         unsafe {
             &*(controller
                 .interrupter()
                 .erdp
                 .read_volatile()
-                .event_ring_dequeue_pointer() as *const Trb)
+                .event_ring_dequeue_pointer() as *const EventTrb)
         }
     }
 
-    fn write_dequeue_pointer(&mut self, controller: &mut Controller, trb: *const Trb) {
+    fn write_dequeue_pointer(&mut self, controller: &mut Controller, trb: *const EventTrb) {
         let ptr = trb as u64;
         controller.interrupter().erdp.update_volatile(|r| {
             r.set_event_ring_dequeue_pointer(ptr);
